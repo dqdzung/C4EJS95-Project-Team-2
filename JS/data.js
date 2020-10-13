f8bf3a0a80a5db7b479ff85e43c1d078bbe01983
hihi-haha-hoho <dinhquangdung1011@gmail.com>
2020-10-13 19:18:01 +0700

    p-wip

--- JS/data.js
+++ JS/data.js
@@ -1,29 +1,41 @@
-let users = [
+const users = [
   { id: "test", password: "123456", name: "Test User", role: "user" },
   { id: "admin", password: "admin", name: "Test Admin", role: "admin" },
 ];
 
-let products = [
-    {
-      name: "Asus VivoBook",
-      model: "A512FL - EJ567T",
-      price: "18.999.000",
-      brand: "Asus",
-      spec: `CPU: Intel Core i7 10510U
-    RAM: 8GB RAM
-    Ổ cứng: 512GB SSD
-    VGA: NVIDIA MX250 2GB
-    Màn hình: 15.6inch FHD
-    HĐH: Win 10
-    Màu: Bạc`,
-      description:
-        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste esse adipisci vero odit ducimus distinctio et sequi minima, ratione beatae consectetur quaerat dolorem animi consequatur aspernatur dolorum dignissimos quos saepe!",
+const products = [
+  {
+    name: "Asus VivoBook",
+    model: "A512FL - EJ567T",
+    price: "18.999.000",
+    brand: "Asus",
+    spec: {
+      cpu: "Intel Core i7 10510U",
+      ram: "8",
+      storage: "512GB SSD",
+      vga: "NVIDIA MX250 2GB",
+      screen: "15.6 FHD",
+      os: "Win 10",
+      color: "Silver",
     },
-    {
-      name: "test item 2",
-      price: "40",
-      brand: "tasd",
-      spec: "lorem askjdhaksjhd",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste esse adipisci vero odit ducimus distinctio et sequi minima, ratione beatae consectetur quaerat dolorem animi consequatur aspernatur dolorum dignissimos quos saepe!",
+  },
+  {
+    name: "Asus VivoBook",
+    model: "A512FL - EJ567T",
+    price: "18.999.000",
+    brand: "Asus",
+    spec: {
+      cpu: "Intel Core i7 10510U",
+      ram: "8",
+      storage: "512GB SSD",
+      vga: "NVIDIA MX250 2GB",
+      screen: "15.6 FHD",
+      os: "Win 10",
+      color: "Silver",
     },
-  ];
-  
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste esse adipisci vero odit ducimus distinctio et sequi minima, ratione beatae consectetur quaerat dolorem animi consequatur aspernatur dolorum dignissimos quos saepe!",
+  },
+];
