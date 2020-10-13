c02766607e2b07ba16a59acf8eb7d128063a97c1
nguyenvanquyen1511 <quyena5nvc@gmail.com>
2020-10-13 20:03:44 +0700

    update

--- JS/data.js
+++ JS/data.js
@@ -38,4 +38,72 @@ const products = [
     description:
       "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste esse adipisci vero odit ducimus distinctio et sequi minima, ratione beatae consectetur quaerat dolorem animi consequatur aspernatur dolorum dignissimos quos saepe!",
   },
+  {
+    name: "Asus VivoBook",
+    model: "A512FL - EJ567T",
+    price: "18.999.000",
+    brand: "Asus",
+    spec: {
+      cpu: "Intel Core i7 10510U",
+      ram: "8",
+      storage: "512GB SSD",
+      vga: "NVIDIA MX250 2GB",
+      screen: "15.6 FHD",
+      os: "Win 10",
+      color: "Silver",
+    },
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste esse adipisci vero odit ducimus distinctio et sequi minima, ratione beatae consectetur quaerat dolorem animi consequatur aspernatur dolorum dignissimos quos saepe!",
+  },
+  {
+    name: "Asus VivoBook",
+    model: "A512FL - EJ567T",
+    price: "18.999.000",
+    brand: "Asus",
+    spec: {
+      cpu: "Intel Core i7 10510U",
+      ram: "8",
+      storage: "512GB SSD",
+      vga: "NVIDIA MX250 2GB",
+      screen: "15.6 FHD",
+      os: "Win 10",
+      color: "Silver",
+    },
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste esse adipisci vero odit ducimus distinctio et sequi minima, ratione beatae consectetur quaerat dolorem animi consequatur aspernatur dolorum dignissimos quos saepe!",
+  },
+  {
+    name: "Asus VivoBook",
+    model: "A512FL - EJ567T",
+    price: "18.999.000",
+    brand: "Asus",
+    spec: {
+      cpu: "Intel Core i7 10510U",
+      ram: "8",
+      storage: "512GB SSD",
+      vga: "NVIDIA MX250 2GB",
+      screen: "15.6 FHD",
+      os: "Win 10",
+      color: "Silver",
+    },
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste esse adipisci vero odit ducimus distinctio et sequi minima, ratione beatae consectetur quaerat dolorem animi consequatur aspernatur dolorum dignissimos quos saepe!",
+  },
+  {
+    name: "Asus VivoBook",
+    model: "A512FL - EJ567T",
+    price: "18.999.000",
+    brand: "Asus",
+    spec: {
+      cpu: "Intel Core i7 10510U",
+      ram: "8",
+      storage: "512GB SSD",
+      vga: "NVIDIA MX250 2GB",
+      screen: "15.6 FHD",
+      os: "Win 10",
+      color: "Silver",
+    },
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste esse adipisci vero odit ducimus distinctio et sequi minima, ratione beatae consectetur quaerat dolorem animi consequatur aspernatur dolorum dignissimos quos saepe!",
+  },
 ];
